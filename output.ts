864094865e16b41c9063876364e3d948e53a3813
RAHUL RUDRA <rahulrudra665@gmail.com>
2025-11-18 21:27:56 +0600

    changes made"

--- output.ts
+++ output.ts
@@ -15,3 +15,58 @@ const formatValue: IFunction = (a) => {
     console.log("The input number is not a string ,number or boolean");
   }
 };
+
+interface IFunction2 {
+  (value1: string | any[] | undefined): number | undefined;
+}
+const getLength: IFunction2 = (value1) => {
+  if (typeof value1 === "string") {
+    const lengthString = value1.length;
+    return lengthString;
+  } else if (Array.isArray(value1)) {
+    const lengthArray = value1.length;
+    return lengthArray;
+  } else {
+    console.log("Invalid Output");
+  }
+};
+
+class Person {
+  name: string;
+  age: number;
+  constructor(nameGiven: string, ageGiven: number) {
+    this.name = nameGiven;
+    this.age = ageGiven;
+  }
+  getDetails() {
+    const formattedString = `Name: ${this.name}, Age: ${this.age}  `;
+    return formattedString;
+  }
+}
+
+const person1 = new Person("John Doe", 30);
+
+interface BooksArray {
+  title: string;
+  rating: number;
+}
+
+function filterByRating(items: BooksArray[]): BooksArray[] {
+  return items.filter((item) => item.rating >= 4);
+}
+
+interface Users {
+  id: number;
+  name: string;
+  email: string;
+  isActive: boolean;
+}
+[];
+
+interface IFunction3 {
+  (usersArrayInput: Users[]): Users[];
+}
+
+const filterActiveUsers: IFunction3 = (usersArrayInput) => {
+  return usersArrayInput.filter((user) => user.isActive);
+};
